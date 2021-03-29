b961a6e20f16eaec01a95fdad1a3a65ca6c7c3cb
nwainwright2 <71286374+nwainwright2@users.noreply.github.com>
2021-03-29 12:34:52 -0400

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,6 @@
+userName = input("Hello! Please enter your facebook username: ")
+userPassword = input("Hello! Please enter your facebook password: ")
+if(userName.lower()=="nick" and userPassword == "P@ssworD!"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
